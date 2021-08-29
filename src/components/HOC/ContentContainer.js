099e71616c39bec2d3ac5f3f970a2ddd55fb12cb
Hanwen Zhang <hzhang8@bu.edu>
2021-08-29 16:21:38 -0400

    commit

--- src/components/HOC/ContentContainer.js
+++ src/components/HOC/ContentContainer.js
@@ -31,22 +31,3 @@ const ContentContainer = (OriginalComponent) => {
 };
 
 export default ContentContainer;
-
-// *Visibility Container Component (<ContentContainer>)
-//   Implement a container component to control the visibility of the above two components
-//   This component should contain logic to alter the visibility of the Counter and To-do List components
-//   (For instance, two checkboxes for each component and when checked/unchecked, display/hide it)
-//   Recommend use HOC as component wrapper to add extra visible prop to each component
-
-//   For HOC implementation:
-//   *App.js
-//     <div className="App">
-//       <ContentContainer />
-//     </div>
-
-//   *ContentContainer.js
-//     <>
-//       ...(logic for visibility control)
-//       <HOCCounter visibie={somethingValue}/>
-//       <HOCTdList />
-//     </>
